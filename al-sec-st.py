2865573b813922f476433602bf79dd1f4ba70ccd
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-04 22:42:55 -0700

    al-sec-st, pr

---
+++ al-sec-st.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import praw
+import pdb
+import re
+import os
+
+
+# Create the Reddit instance
+reddit = praw.Reddit('bot1')
+
+# and login
+#reddit.login(REDDIT_USERNAME, REDDIT_PASS)
+
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+local_subs = open("alabama.dat", "r")
+text_file = open("standardsubs.dat", "r")
+subs = local_subs.read().split('\n')
+ssubs = text_file.read().split('\n')
+subs.extend(ssubs)
+
+# Get the top values from our subreddit
+def searchAndPost(sub):
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot(limit=50):
+        #print(submission.title)
+
+        # If we haven't replied to this post before
+        if submission.id not in posts_replied_to:
+
+            # Do a case insensitive search
+            terms = ['too poor to vote', 'Alabama Secretary of State', 'John Merrill']
+            for term in terms:
+                 search(term, submission);
+
+def search(term, submission):
+    if re.search(term, submission.title, re.IGNORECASE):
+        # Reply to the post
+        text = ("[&#9733;&#9733;&#9733; Register to Vote &#9733;&#9733;&#9733;](https://www.alabamainteractive.org/sos/voter_registration/voterRegistrationWelcome.action) by May 21, 2018 \n\n"
+            "[Find your polling place](https://myinfo.alabamavotes.gov/VoterView/PollingPlaceSearch.do) \n\n"
+
+            "Merrill is currently unopposed in 2018. Know someone who should [run](https://www.runforoffice.org/elected_offices/43302-alabama-secretary-of-state/interest_form)? \n\n "
+
+            "Primary Election: June 5, 2018 | General Election: November 6, 2018 \n\n"
+
+            "^(I'm a bot and I'm learning. Let me know how I can do better. I'll add candidates who will represent working-class people instead of billionaire political donors.)")
+        print("Bot replying to : ", submission.title)
+        try:
+            submission.reply(text)
+        except Exception:
+            print("Error : ", submission.title)
+            pass
+
+        # Store the current id into our list
+        posts_replied_to.append(submission.id)
+
+for sub in subs:
+     print(sub)
+     searchAndPost(sub);
+
+# Write our updated list back to the file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
+
+text_file.close()
+local_subs.close()
