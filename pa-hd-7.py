9df27fe404d0396af835aca7f4c4466c36cb47ce
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-08 13:14:16 -0700

    pa-7

---
+++ pa-hd-7.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import praw
+import pdb
+import re
+import os
+
+# Create the Reddit instance
+reddit = praw.Reddit('bot1')
+
+# and login
+#reddit.login(REDDIT_USERNAME, REDDIT_PASS)
+
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+local_subs = open("pennsylvania.dat", "r")
+text_file = open("standardsubs.dat", "r")
+subs = local_subs.read().split('\n')
+ssubs = text_file.read().split('\n')
+subs.extend(ssubs)
+
+# Get the top values from our subreddit
+def searchAndPost(sub):
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot(limit=50):
+        #print(submission.title)
+
+        # If we haven't replied to this post before
+        if submission.id not in posts_replied_to:
+
+            # Do a case insensitive search
+            terms = ['joe billie', 'pa-7', 'pa-07', 'meehan']
+            for term in terms:
+                 search(term, submission);
+
+def search(term, submission):
+    if re.search(term, submission.title, re.IGNORECASE):
+        # Reply to the post
+        vote_link = ("[&#9733;&#9733;&#9733; Register To Vote &#9733;&#9733;&#9733;](https://www.pavoterservices.pa.gov/Pages/VoterRegistrationApplication.aspx) \n\n"
+        "[Find your polling place](https://www.pavoterservices.pa.gov/Pages/PollingPlaceInfo.aspx) \n\n")
+
+        dems = ("[**Molly Sheehan**](https://mollysheehan.org/) is running to represent Pennsylvania District 7 in the United States Congress. \n\n"
+        "[Facebook](https://www.facebook.com/sheehanforcongress/) | "
+        "[Twitter](https://twitter.com/pennsymolly) | "
+        "[Volunteer](https://mollysheehan.org/take-action/) | "
+        "[Donate](https://secure.actblue.com/donate/molly-sheehan) \n\n"
+        "Sheehan supports single-payer health care, public schools, paid family leave, renewable energy, campaign finance reform, LGBTQ equality, vote by mail, and automatic voter registration. \n\n\n"
+
+        "[**Paul Perry**](http://perryforpa.com/) is running to represent Pennsylvania District 7 in the United States Congress. \n\n"
+        "[Facebook](https://www.facebook.com/PaulPerryforCongress/) | "
+        "[Twitter](https://twitter.com/PerryForPA) | "
+        "[Volunteer](http://perryforpa.com/volunteer/) | "
+        "[Donate](https://secure.actblue.com/donate/paulperry2018) \n\n"
+        "Perry supports Medicare for all, a living wage, paid family leave, renewable energy, campaign finance reform, police de-escalation training, and automatic voter registration. \n\n\n"
+
+        map = ("[Map of Pennsylvania House District 7](https://www.govtrack.us/congress/members/PA/7) \n\n")
+
+        with open('disclaimer.txt', 'r') as myfile:
+            disclaimer=myfile.read().replace('\n', '')
+
+        text = '\n'.join([vote_link, dems, map, disclaimer])
+        print("Bot replying to : ", submission.title)
+        try:
+            submission.reply(text)
+        except Exception:
+            print("Error : ", submission.title)
+            pass
+
+        # Store the current id into our list
+        posts_replied_to.append(submission.id)
+
+for sub in subs:
+     print(sub)
+     searchAndPost(sub);
+
+# Write our updated list back to the file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
+
+text_file.close()
+local_subs.close()
