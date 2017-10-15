1a978727f324d6eb58e8ff3088f4486dbf78618b
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-14 21:25:17 -0700

    rodneydavis

--- rodneydavis.py
+++ rodneydavis.py
@@ -22,9 +22,13 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
-subs = ['uiuc', 'politicalrevolutionil', 'chicagosunauto', 'republicansaredicks', 'news', 'indepthstories', 'democrats', 'chapotraphouse', 'bluemidterm2018', 'enoughtrumpspam', 'liberal', 'political_revolution', 'keepournetfree', 'thehillauto', 'cornbreadliberals', 'thenewcoldwar', 'esist', 'waexauto', 'unremovable', 'good_cake', 'technology', 'autonewspaper', 'wayofthebern', 'sandersforpresident', 'autotldr', 'marchagainsttrump', 'politicalvideo', 'goodlongposts', 'badgovnofreedom', 'libs', 'democracy', 'stupid_watergate', 'fcc', 'netneutrality', 'worldnews', 'nottheonion', 'BreakingNews24hr', 'newsbotbot', 'impeach_trump', 'fuckthealtright', 'collapse', 'environment', 'inthenews', 'hotandtrending', 'keep_track', 'thecolorisblue', 'PoliticalVideos', 'climate', 'cnet_all_rss', 'women', 'newsy', 'cnnauto', 'tytpolitics', 'huffpoauto', 'cbsauto', 'greed', 'watchingcongress', 'restorethefourth', 'trussiagate', '538auto', 'theconstitution', 'pancakepalpatine', 'geprnotes', 'progressive', 'datauncensored', 'skydtech']
+local_subs = open("illinois.dat", "r")
+text_file = open("standardsubs.dat", "r")
+subs = local_subs.read().split('\n')
+ssubs = text_file.read().split('\n')
+subs.extend(ssubs)
 
-# Get the top 500 values from our subreddit
+# Get the top values from our subreddit
 def searchAndPost(sub):
     subreddit = reddit.subreddit(sub)
     for submission in subreddit.hot(limit=50):
@@ -41,13 +45,34 @@ def searchAndPost(sub):
 def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
-        text = ("[&#9733;&#9733;&#9733; Register To Vote &#9733;&#9733;&#9733;](https://ova.elections.il.gov/Step0.aspx) \n\n"
+        text = ("[&#9733;&#9733;&#9733; Register To Vote &#9733;&#9733;&#9733;](https://ova.elections.il.gov/Step0.aspx) by February 20, 2018 \n\n"
+            "[Sign up to vote by mail](https://www.elections.il.gov/VotingInformation/VotingByMail.aspx) \n\n\n"
+
             "[**David Gill**](https://www.davidgill2018.com/platform) is running against Rodney Davis. \n\n"
-            "[Donate](https://secure.actblue.com/contribute/page/doctorgillforcongress) | "
             "[Facebook](https://www.facebook.com/DavidGill2018/) | "
-            "[Twitter](https://twitter.com/davidgill2018) \n\n"
+            "[Twitter](https://twitter.com/davidgill2018) | "
+            "[Volunteer](https://www.davidgill2018.com/get-involved) | "
+            "[Donate](https://secure.actblue.com/contribute/page/doctorgillforcongress) \n\n"
             "Gill supports universal health care, public schools, living wages, affordable college, renewable energy, standing with our international allies, LGBTQ equality, and background checks on every gun sale. \n\n\n"
 
+            "[**Jon Ebel**](http://www.jonebel.com/) is running against Rodney Davis. \n\n"
+            "[Facebook](https://www.facebook.com/Friends-of-Jonathan-Ebel-229030640931027/) | "
+            "[Twitter](https://twitter.com/jonathan_ebel) | "
+            "[Volunteer](http://www.jonebel.com/get-involved/) | "
+            "[Donate](https://contributions.jonebel.com/) \n\n"
+
+            "[**Betsy Dirksen Londrigan**](http://www.betsydirksenlondrigan.com/) is running against Rodney Davis. \n\n"
+            "[Facebook](https://www.facebook.com/BetsyDirksenLondrigan/) | "
+            "[Twitter](https://twitter.com/BetsyforIL) | "
+            "[Volunteer](http://betsydirksenlondrigan.com/index.php/volunteer) | "
+            "[Donate](https://secure.actblue.com/donate/betsydirksenlondriganforillinois) \n\n"
+
+            "[**Erik Jones**](https://erikjonesforcongress.com/) is running against Rodney Davis. \n\n"
+            "[Facebook](https://www.facebook.com/ErikJones4IL/) | "
+            "[Twitter](https://twitter.com/ErikJones4IL) | "
+            "[Donate](https://secure.actblue.com/donate/erikjones) \n\n"
+
+            "Primary Election: March 20, 2018 | General Election: November 6, 2018 \n\n"
             "[Map of Illinois District 13](https://www.govtrack.us/congress/members/IL/13) \n\n"
 
             "^(I'm a bot and I'm learning. Let me know if I can do better. It's a lot of "
@@ -66,3 +91,6 @@ for sub in subs:
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
+
+text_file.close()
+local_subs.close()
