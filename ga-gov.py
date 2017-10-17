d94c8f403d532aa01c1645404d683e2f40e2b666
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-16 21:07:56 -0700

    ugh

--- ga-gov.py
+++ ga-gov.py
@@ -38,7 +38,8 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
             # Do a case insensitive search
             terms = ['nathan deal', 'georgia governor', 'governor of georgia']
-            search(term, submission)
+            for term in terms:
+                 search(term, submission);
 
 def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
