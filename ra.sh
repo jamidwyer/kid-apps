d238b37d03d02a5dadfe120a7ec1c0685d4c69fc
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-17 20:11:38 -0700

    chaffetz

--- ra.sh
+++ ra.sh
@@ -15,8 +15,8 @@ python va-st-hd-58.py
 python va-st-hd-13.py
 python va-st-hd-42.py
 python ny-manhattan-da.py
-python strange.py
 python chaffetz.py
+python strange.py
 python valadao.py
 python paulsen.py
 python royce.py
