663c0e9dff25cba081f6c7d39cb7117ca76bf3d6
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-19 16:06:24 -0700

    alabama, cruz, pa-18, pr

--- pa-hd-18.py
+++ pa-hd-18.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['tim murphy', 'pa-18', 'congressman to resign after report alleged he asked woman to have an abortion', 'Congressman Resigns After Allegedly Urging Mistress to Get an Abortion', 'Republican congressman who urged mistress to get an abortion refuses to quit']
+            terms = ['tim murphy', 'pa-18', 'bob solomon', 'congressman to resign after report alleged he asked woman to have an abortion', 'Congressman Resigns After Allegedly Urging Mistress to Get an Abortion', 'Republican congressman who urged mistress to get an abortion refuses to quit']
             for term in terms:
                  search(term, submission);
 
