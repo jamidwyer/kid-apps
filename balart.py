e4fedc7b1a4d0d06d9e122f4d4931fbae4173430
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-23 09:25:32 -0700

    gillespie, ryan, balart, illinois, ryan, subs

--- balart.py
+++ balart.py
@@ -39,7 +39,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['balart', 'fl-27']
+            terms = ['balart', 'fl-25']
             for term in terms:
                  search(term, submission);
 
