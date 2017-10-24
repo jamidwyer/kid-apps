716314b71e21fb9ba04993939d98872f4246eae0
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-24 08:45:48 -0700

    chabot, lamalfa

--- chabot.py
+++ chabot.py
@@ -39,7 +39,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['steve chabot', 'rep. chabot', 'rep chabot', 'representative chabot', 'congressman chabot', 'oh-1', 'oh-01']
+            terms = ['steve chabot', 'rep. chabot', 'rep chabot', 'representative chabot', 'congressman chabot', 'oh-1', 'oh-01', '136 Democrats support Medicare-For-All']
             for term in terms:
                  search(term, submission);
 
