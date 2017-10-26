c2fee9e2063baa9e47af54c3f9147d5511477364
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-10-25 21:52:52 -0700

    barrasso, reichert

--- barrasso.py
+++ barrasso.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['barrasso', 'erik prince', 'Senate Committee Advances Controversial Trump EPA Nominee', 'Pushing Ahead with a Health Care Deal']
+            terms = ['barrasso', 'erik prince', 'Senate Environment Committee Approves Toxic EPA Nominee', 'Senate Committee Advances Controversial Trump EPA Nominee', 'Pushing Ahead with a Health Care Deal']
             for term in terms:
                  search(term, submission);
 
