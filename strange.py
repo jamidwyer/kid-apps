07f654a3d2a24f5329f45cb6b4bedee49cea05bc
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-10 10:17:48 -0800

    reddit says candidate info is spam

--- strange.py
+++ strange.py
@@ -25,11 +25,11 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
-local_subs = open("alabama.dat", "r")
+//local_subs = open("alabama.dat", "r")
 text_file = open("standardsubs.dat", "r")
-subs = local_subs.read().split('\n')
-ssubs = text_file.read().split('\n')
-subs.extend(ssubs)
+//subs = local_subs.read().split('\n')
+subs = text_file.read().split('\n')
+//subs.extend(ssubs)
 
 # Get the top values from our subreddit
 def searchAndPost(sub):
