e7bc74bb5a37b5bf6e6ce5597cc0e517ba2508ea
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-11 22:01:02 -0800

    flake

--- flake.py
+++ flake.py
@@ -44,10 +44,10 @@ def searchAndPost(sub):
 def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
-            text = ("Arizona 2018 Election \n\n"
-                "[Voter Registration Deadline](https://servicearizona.com/webapp/evoter/register?execution=e1s2): July 30, 2018 \n\n"
-                "[Primary Election](https://www.vote.org/absentee-ballot/): August 28, 2018 \n\n"
-                "[General Election](https://www.vote.org/absentee-ballot/): November 6, 2018 \n\n")
+        text = ("Arizona 2018 Election \n\n"
+            "[Voter Registration Deadline](https://servicearizona.com/webapp/evoter/register?execution=e1s2): July 30, 2018 \n\n"
+            "[Primary Election](https://www.vote.org/absentee-ballot/): August 28, 2018 \n\n"
+            "[General Election](https://www.vote.org/absentee-ballot/): November 6, 2018 \n\n")
         print("Bot replying to : ", submission.title)
         try:
             submission.reply(text)
