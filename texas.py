cb323e425e8fed968071f02e76f36ce312997ab3
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-15 12:26:37 -0800

    tx

--- texas.py
+++ texas.py
@@ -36,9 +36,11 @@ def searchAndPost(sub):
 
         # If we haven't replied to this post before
         if submission.id not in posts_replied_to:
+
             # Do a case insensitive search
             terms = ['be persuaded on climate change. So now what']
-            search(term);
+            for term in terms:
+                search(term);
 
 def search(term, submission, blend_in, fb_link, twitter_link, donate_link):
     if re.search(term, submission.title, re.IGNORECASE):
