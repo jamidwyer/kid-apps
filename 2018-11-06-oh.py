7b274f50a4344771d3b41449ebf7fb6f9f12d354
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-17 23:06:45 -0800

    oh, wi

--- 2018-11-06-oh.py
+++ 2018-11-06-oh.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['bill o\'neill', '50 very attractive females', 'steve chabot', 'rep. chabot', 'rep chabot', 'representative chabot', 'congressman chabot', 'oh-1', 'oh-01', 'Republican county in Ohio just flipped nine seats blue', '136 Democrats support Medicare-For-All', 'ken harbaugh', 'josh mandel', 'kasich', 'ohio governor', 'oh gov', 'oh governor\'s', 'jerry springer', 'Mary Taylor']
+            terms = ['Representative Bill Johnson', 'bill o\'neill', '50 very attractive females', 'steve chabot', 'rep. chabot', 'rep chabot', 'representative chabot', 'congressman chabot', 'oh-1', 'oh-01', 'Republican county in Ohio just flipped nine seats blue', '136 Democrats support Medicare-For-All', 'ken harbaugh', 'josh mandel', 'kasich', 'ohio governor', 'oh gov', 'oh governor\'s', 'jerry springer', 'Mary Taylor']
             for term in terms:
                 search(term, submission);
 
