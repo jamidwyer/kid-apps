e40492fa68f50068ac2f79691252731047750d90
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-19 18:05:14 -0800

    pa, tx

--- ra.sh
+++ ra.sh
@@ -2,15 +2,15 @@
 
 python strange.py
 python 2018-02-13-ok.py
+python 2018-03-13-pa.py
 python 2018-11-06-tx.py
 python 2018-11-06-il.py
 python 2018-11-06-oh.py
 python 2018-11-06-nc.py
-python pennsylvania.py
+python 2018-11-06-pa.py
 python 2018-11-06-sc.py
 python 2018-11-06-nj.py
 python 2018-11-06-nv.py
-python nevada.py
 python 2018-11-06-ca.py
 python virginia.py
 python 2018-11-06-ok.py
