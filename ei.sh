0cfdf45809fed1f72d9ab495c804a09420fc953b
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-21 18:35:34 -0800

    me

--- ei.sh
+++ ei.sh
@@ -16,7 +16,7 @@ python 2018-11-06-ca.py
 python virginia.py
 python 2018-11-06-ok.py
 python larryhogan.py
-python lepage.py
+python 2018-11-06-me.py
 python 2018-11-06-ny.py
 python 2018-11-06-ut.py
 python 2018-11-06-mi.py
