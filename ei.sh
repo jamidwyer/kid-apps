4a29c20ba63ddfbfbe86b521d240322667ea8a44
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-28 22:43:50 -0800

    mn

--- ei.sh
+++ ei.sh
@@ -28,6 +28,7 @@ python 2018-11-06-mo.py
 python 2018-11-06-ks.py
 python 2018-11-06-wa.py
 python 2018-11-06-az.py
+python 2018-11-06-mn.py
 python 2018-11-06-wi.py
 python 2018-11-06-nh.py
 python 2018-11-06-la.py
