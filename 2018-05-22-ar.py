6137d74e89b14b03e541d1a33836b8da99342166
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-11-28 17:59:45 -0800

    ar special

---
+++ 2018-05-22-ar.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import praw
+import pdb
+import re
+import os
+
+
+# Create the Reddit instance
+reddit = praw.Reddit('bot1')
+
+# and login
+#reddit.login(REDDIT_USERNAME, REDDIT_PASS)
+
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+local_subs = open("arkansas.dat", "r")
+text_file = open("standardsubs.dat", "r")
+subs = local_subs.read().split('\n')
+ssubs = text_file.read().split('\n')
+subs.extend(ssubs)
+
+
+# Get the top values from our subreddit
+def searchAndPost(sub):
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot(limit=100):
+        #print(submission.title)
+
+        # If we haven't replied to this post before
+        if submission.id not in posts_replied_to:
+
+            # Do a case insensitive search
+            terms = ['progressive friend is running for state senate in our deeply red state']
+            for term in terms:
+                 search(term, submission);
+
+def search(term, submission):
+    if re.search(term, submission.title, re.IGNORECASE):
+        # Reply to the post
+        text = ("Arkansas Special Election \n\n"
+            "Primary Election: [Register by](https://www.sos.arkansas.gov/elections/Pages/voterRegistration.aspx): January 14, 2018 | [Vote by](https://www.voterview.ar-nova.org/VoterView/Home.do): February 13, 2018 \n\n"
+            "General Election: [Register by](https://www.sos.arkansas.gov/elections/Pages/voterRegistration.aspx): June 22, 2018 | [Vote by](https://www.voterview.ar-nova.org/VoterView/Home.do): May 22, 2018 \n\n")
+        print("Bot replying to : ", submission.title)
+        try:
+            submission.reply(text)
+        except Exception:
+            print("Error : ", submission.title)
+            pass
+
+
+        # Store the current id into our list
+        posts_replied_to.append(submission.id)
+
+for sub in subs:
+     print(sub)
+     searchAndPost(sub);
+
+# Write our updated list back to the file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
+
+text_file.close()
+local_subs.close()
