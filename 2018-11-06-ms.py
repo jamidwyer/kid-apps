2941a33a06815c5aecdcf5e4cb5bea0c9e21e165
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-12-13 12:15:17 -0800

    ms

--- 2018-11-06-ms.py
+++ 2018-11-06-ms.py
@@ -47,9 +47,9 @@ def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
         text = ("Mississippi 2018 Election \n\n"
-            "[Primary Election Registration Deadline](http://www.sos.ms.gov/Elections-Voting/Pages/Voter-Registration-Information.aspx): 2018 \n\n"
-            "[Primary Election](): 2018 \n\n"
-            "[General Election Registration Deadline](http://www.sos.ms.gov/Elections-Voting/Pages/Voter-Registration-Information.aspx): 2018 \n\n"
+            "[Primary Election Registration Deadline](http://www.sos.ms.gov/Elections-Voting/Pages/Voter-Registration-Information.aspx): May 7, 2018 \n\n"
+            "[Primary Election](): June 5, 2018 \n\n"
+            "[General Election Registration Deadline](http://www.sos.ms.gov/Elections-Voting/Pages/Voter-Registration-Information.aspx): October 8, 2018 \n\n"
             "[General Election](): November 6, 2018 \n\n")
         print("Bot replying to : ", submission.title)
         try:
