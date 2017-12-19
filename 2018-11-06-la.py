01f700d81dc19b37e3941c60e0e1e31c227e3269
Jami Dwyer <jdwyer@phylosbioscience.com>
2017-12-18 21:43:26 -0800

    la, oh

--- 2018-11-06-la.py
+++ 2018-11-06-la.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['The end of American prison visits', 'most incarcerated state', 'scalise', 'this nation to the telecom lobby for the low, low price', 'clay higgins', 'Louisiana Prison Percentage Full by Month']
+            terms = ['Prayers, Proselytizing by Louisiana School District', 'll Stop When Someone Makes Me Stop', 'Louisiana superintendent refuses', 'The end of American prison visits', 'most incarcerated state', 'scalise', 'this nation to the telecom lobby for the low, low price', 'clay higgins', 'Louisiana Prison Percentage Full by Month']
             for term in terms:
                  search(term, submission);
 
