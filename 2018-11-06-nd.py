b2bba53fc353681928fa327842a6aec5a956c131
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-11 08:08:20 -0800

    nd

--- 2018-11-06-nd.py
+++ 2018-11-06-nd.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Can humane prisons work in America']
+            terms = ['congressman kramer', 'Can humane prisons work in America']
             for term in terms:
                  search(term, submission);
 
