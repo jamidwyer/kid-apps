dd14db847ab34dc0822d920779bec4d04b5ed84d
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-21 15:35:22 -0800

    ny, sd

--- 2018-11-06-sd.py
+++ 2018-11-06-sd.py
@@ -4,6 +4,7 @@ import pdb
 import re
 import os
 
+
 # Create the Reddit instance
 reddit = praw.Reddit('bot1')
 
@@ -22,11 +23,12 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
-local_subs = open("newyork.dat", "r")
+local_subs = open(".dat", "r")
 text_file = open("standardsubs.dat", "r")
 subs = local_subs.read().split('\n')
 ssubs = text_file.read().split('\n')
 subs.extend(ssubs)
+
 
 # Get the top values from our subreddit
 def searchAndPost(sub):
@@ -38,18 +40,18 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['hoping for a terrorist attack to counter 2018 elections', 'nyc women\'s march', 'A man who knows. Women', 'Trump suggests that a terror attack could help Republicans in the 2018 midterms', 'terror attack could save him and GOP from 2018 election bloodbath', 'New York City homeless shelter faces resistance', 'New York to look at legalizing recreational marijuana', 'Marijuana legalization may be coming to New York sooner rather than later', 'ny27', 'New York City\'s fossil fuel divestment could spur global shift', 'New York City Declares War on the Oil Industry', 'New York Finally Consider Legalizing Cannabis', 'New York Police Union Sues to Stop Release of Body Camera Videos', 'Fund Early Voting', 'Republicans Poised for Collapse in New York', 'sara idleman', 'New York introduces its own net neutrality bill', 'barely disguised reward for billionaire donors', 'GOP tax plan the result of a runaway campaign finance system', 'peter king', 'Net Neutrality Supporters At NYC Verizon Store', 'House Seats Democrats Are Hoping to Win in 2018', 'GOP Tax Bill Displays American Oligarchy', 'katko', '^(?!.*burkina faso).*faso.*$', 'daniel donovan', 'dan donovan', 'ny-11', 'rep. donovan', 'rep donovan', 'representative donovan', 'congressman donovan', 'michael grimm', 'GOP incumbent over Grimm']
+            terms = ['jail for anyone replicating South Dakota state seal']
             for term in terms:
                  search(term, submission);
 
 def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
-
-        text = ("New York 2018 Election \n\n"
-            "[Primary Election Date:](https://nyc.pollsitelocator.com/search): June 26, 2018 \n\n"
-            "[General Election Registration Deadline](https://voterreg.dmv.ny.gov/MotorVoter/): October 12, 2018 \n\n"
-            "[General Election Date:](https://nyc.pollsitelocator.com/search): November 6, 2018")
+        text = ("South Dakota 2018 Election \n\n"
+            "[Primary Voter Registration Deadline](): 2018 \n\n"
+            "[Primary Election Date](): 2018 \n\n"
+            "[General Election Registration Deadline](): 2018 \n\n"
+            "[General Election](): November 6, 2018 \n\n")
         print("Bot replying to : ", submission.title)
         try:
             submission.reply(text)
