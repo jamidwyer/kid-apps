3d13bf506da2c0cc9a4396b7ce711da1bcbe4569
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-22 16:15:13 -0800

    pa, ga, mi, mt, or, ut, vt

--- 2018-11-06-vt.py
+++ 2018-11-06-vt.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Governor Scott sticks it to the taxpayers', 'GOP in Vermont']
+            terms = ['Vermont has officially legalized cannabis', 'Governor Scott sticks it to the taxpayers', 'GOP in Vermont']
             for term in terms:
                  search(term, submission);
 
