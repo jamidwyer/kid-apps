4d703e197b6ddc2e82803622c7d2a54ded293947
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-24 11:47:41 -0800

    mo, az, fl, ga, ny, va

--- 2018-04-03-mo.py
+++ 2018-04-03-mo.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Jeanie Ames']
+            terms = ['parkway school board', 'Jeanie Ames']
             for term in terms:
                  search(term, submission);
 
