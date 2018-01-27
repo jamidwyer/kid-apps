603b5b2561dffca5b76c52af8247ba235aa3dccd
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-27 10:22:00 -0800

    fl, in, ut

--- 2018-11-06-in.py
+++ 2018-11-06-in.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Laid-off factory workers feel betrayed by Trump', 'Indiana Green Party', 'in-09', 'pelath', 'Joe Donnelly', '2018 elections in Indiana', 'Better Know a State: Indiana']
+            terms = ['Indiana Eyes Medical Marijuana Legalization', 'Laid-off factory workers feel betrayed by Trump', 'Indiana Green Party', 'in-09', 'pelath', 'Joe Donnelly', '2018 elections in Indiana', 'Better Know a State: Indiana']
             for term in terms:
                  search(term, submission);
 
