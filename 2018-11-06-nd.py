569608675a586779c27f39124894a5f7bd0427ad
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-01-31 18:27:56 -0800

    nd

--- 2018-11-06-nd.py
+++ 2018-11-06-nd.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Roll Back Fracking Standards', 'rep kevin cramer', 'congressman kramer', 'Can humane prisons work in America']
+            terms = ['emineth', 'heitkamp', 'Roll Back Fracking Standards', 'rep kevin cramer', 'congressman kramer', 'Can humane prisons work in America']
             for term in terms:
                  search(term, submission);
 
