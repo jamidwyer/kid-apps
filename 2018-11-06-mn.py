d922c6c75140fbf7063738e9f114cf22512752c0
Jami Dwyer <jdwyer@phylosbioscience.com>
2018-02-09 17:20:58 -0800

    mn, oh, pa

--- 2018-11-06-mn.py
+++ 2018-11-06-mn.py
@@ -54,7 +54,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['michele bachmann', 'Poison a Minnesota Wilderness', 'Am I Being Called to Do This Now', 'adam jennings', 'Pro-Trump Republican Rep Just Deleted Her Twitter', 'mary franson', 'Lt. Gov. Tina Smith', 'plans to gerrymander the Electoral College in three states', 'erik paulsen', 'rep. paulsen', 'rep paulsen']
+            terms = ['all-in on defense in Minnesota', 'michele bachmann', 'Poison a Minnesota Wilderness', 'Am I Being Called to Do This Now', 'adam jennings', 'Pro-Trump Republican Rep Just Deleted Her Twitter', 'mary franson', 'Lt. Gov. Tina Smith', 'plans to gerrymander the Electoral College in three states', 'erik paulsen', 'rep. paulsen', 'rep paulsen']
             for term in terms:
                  search(term, submission);
 
