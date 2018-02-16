abde9168d00c34a51b9b5dd6d902571a524c9bb3
Jami Dwyer <jamidwyer@yahoo.com>
2018-02-15 20:34:03 -0800

    fl, az

--- 2018-04-24-az.py
+++ 2018-04-24-az.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
 
 
             # Do a case insensitive search
-            terms = ['New Arizona Tracking Poll', 'Sign of Change for Politicians in Arizona', 'Start Electing the Working-Class to Congress', '^(?!.*sell westbrook).*westbrook.*$', 'Trump of 2018 bloodbath', 'pressured female subordinates to bear his surrogate children', 'trent franks', 'rep. franks', 'rep franks', 'congressman franks', 'representative franks']
+            terms = ['BWestbrookAZ8', 'New Arizona Tracking Poll', 'Sign of Change for Politicians in Arizona', 'Start Electing the Working-Class to Congress', '^(?!.*sell westbrook).*westbrook.*$', 'Trump of 2018 bloodbath', 'pressured female subordinates to bear his surrogate children', 'trent franks', 'rep. franks', 'rep franks', 'congressman franks', 'representative franks']
             for term in terms:
                  search(term, submission);
 
