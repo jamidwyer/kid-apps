33bd68d178b885712dbb48bf3cd3d8d88d6b0d06
Jami Dwyer <jamidwyer@yahoo.com>
2018-02-20 19:08:46 -0800

    az, pa, sd

--- 2018-11-06-sd.py
+++ 2018-11-06-sd.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['Banned in South Dakota', 'Democrat with a chance in South Dakota', 'South Dakota Governor', 'jail for anyone replicating South Dakota state seal']
+            terms = ['Gerrymandering a problem in South Dakota', 'Banned in South Dakota', 'Democrat with a chance in South Dakota', 'South Dakota Governor', 'jail for anyone replicating South Dakota state seal']
             for term in terms:
                  search(term, submission);
 
