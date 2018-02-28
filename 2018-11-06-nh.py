fd503e2d8a4d1fe74937a6dc31ceb953cd6e2590
Jami Dwyer <jamidwyer@yahoo.com>
2018-02-27 18:30:19 -0800

    nh

--- 2018-11-06-nh.py
+++ 2018-11-06-nh.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['spagnuolo', 'New Hampshire House votes', 'New Hampshire Lawmakers', 'New Hampshire voter fraud claim']
+            terms = ['New Hampshire seat in Trump district', 'spagnuolo', 'New Hampshire House votes', 'New Hampshire Lawmakers', 'New Hampshire voter fraud claim']
             for term in terms:
                  search(term, submission);
 
