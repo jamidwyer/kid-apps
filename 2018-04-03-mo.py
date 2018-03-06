be08843c7069f98e965d8ae0fb0d18b8bd486602
Jami Dwyer <jamidwyer@yahoo.com>
2018-03-06 14:34:19 -0800

    var

--- 2018-04-03-mo.py
+++ 2018-04-03-mo.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['parkway school board', 'Jeanie Ames']
+            terms = ['parkway board candidate', 'parkway school board', 'Jeanie Ames']
             for term in terms:
                  search(term, submission);
 
