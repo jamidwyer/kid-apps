0d43303d880e14e592dbeb2a0d9d5809eec9eaa4
Jami Dwyer <jamidwyer@yahoo.com>
2018-03-23 21:46:58 -0700

    la, nd, tn

--- ei.sh
+++ ei.sh
@@ -16,35 +16,44 @@ python 2018-04-24-ny.py #4-14
 python 2018-11-06-pa.py #4-16
 python 2018-05-22-ar.py #4-22
 python 2018-11-06-ga.py #4-23
+python 2018-11-06-ky.py #4-23
+python 2018-11-06-ar.py #4-23
+python 2018-11-06-or.py #4-24
 python 2018-11-06-ne.py #5-4
 python 2018-11-06-ms.py #5-7
 python 2018-11-06-mt.py
 python 2018-11-06-nm.py
-python 2018-11-06-sc.py
-python 2018-11-06-nj.py
-python 2018-11-06-nv.py
+python 2018-11-06-nj.py #5-13
+python 2018-11-06-sc.py #5-13
+python 2018-11-06-sd.py #5-13
+python 2018-11-06-nv.py #5-15
 python 2018-11-06-ca.py
-python 2018-11-06-al.py
-python 2018-11-06-va.py
-python 2018-11-06-ok.py
+python 2018-11-06-al.py #5-21
+python 2018-11-06-va.py #5-21
+python 2018-11-06-ia.py #5-25
+python 2018-11-06-ok.py #6-2
 python 2018-11-06-md.py #6-5
 python 2018-11-06-me.py #6-12
+python 2018-11-06-nd.py #6-12
 python 2018-11-06-ut.py #6-19
 python 2018-11-06-ny.py #6-26
+python 2018-11-06-co.py #6-26
 python 2018-11-06-tn.py #7-3
-python 2018-11-06-ut.py
-python 2018-11-06-mo.py 
+python 2018-11-06-mi.py #7-9
+python 2018-11-06-mo.py #7-11
 python 2018-11-06-ks.py #7-17
-python 2018-11-06-ia.py
-python 2018-11-06-wa.py
+python 2018-11-06-ak.py #7-22
+python 2018-11-06-wa.py #7-30
 python 2018-11-06-fl.py
 python 2018-11-06-az.py
-python 2018-11-06-hi.py
+python 2018-11-06-hi.py #8-4
+python 2018-11-06-wy.py #8-6
 python 2018-11-06-de.py #8-11
 python 2018-11-06-ri.py #8-12
 python 2018-11-06-vt.py #8-14
 python 2018-11-06-ma.py #8-29
 python 2018-11-06-mn.py #8-14
+python 2018-11-06-ct.py #8-14
 python 2018-11-06-wi.py
 python 2018-11-06-nh.py #9-11
 python 2018-11-06-tx.py #10-9
