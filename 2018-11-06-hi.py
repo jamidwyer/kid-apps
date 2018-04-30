50a44f43996ea0b0727c7cc5092175c9591d1cdb
Jami Dwyer <jamidwyer@yahoo.com>
2018-04-29 17:04:41 -0700

    hi, tn

--- 2018-11-06-hi.py
+++ 2018-11-06-hi.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['hawaii legislature', 'Hawaii 2018 Ballot', 'gabbard', 'Ending Federal Marijuana Prohibition Act', 'Hawaiian Politician Is Introducing a Bill', 'Hawaii Expects Adult Use Cannabis Legalization', 'kaniela']
+            terms = ['A Hawaiian island got about 50 inches of rain in 24 hours', 'hawaii legislature', 'Hawaii 2018 Ballot', 'gabbard', 'Ending Federal Marijuana Prohibition Act', 'Hawaiian Politician Is Introducing a Bill', 'Hawaii Expects Adult Use Cannabis Legalization', 'kaniela']
             for term in terms:
                  search(term, submission);
 
