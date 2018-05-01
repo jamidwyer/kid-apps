5f7533d41c58b99da5abbef526fc89f80eacfbcc
Jami Dwyer <jamidwyer@yahoo.com>
2018-05-01 00:29:21 -0700

    var

--- 2018-11-06-ks.py
+++ 2018-11-06-ks.py
@@ -45,9 +45,9 @@ def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
         text = ("Kansas 2018 Election \n\n"
-            "[Primary Election Voter Registration Deadline](https://www.kdor.ks.gov/apps/voterreg/default.aspx): July 17, 2018 \n\n"
+            "[Primary Election Voter Registration Deadline](https://ksvotes.org/): July 17, 2018 \n\n"
             "[Primary Election](http://www.kssos.org/forms/elections/AV1.pdf): August 7, 2018 \n\n"
-            "[General Election Voter Registration Deadline](https://www.kdor.ks.gov/apps/voterreg/default.aspx): October 16, 2018 \n\n"
+            "[General Election Voter Registration Deadline](https://ksvotes.org/): October 16, 2018 \n\n"
             "[General Election](http://www.kssos.org/forms/elections/AV1.pdf): November 6, 2018 \n\n")
         print("Bot replying to : ", submission.title)
         try:
