b763768ee42d303c9704e77617161008fb2f6be0
Jami Dwyer <jamidwyer@yahoo.com>
2018-05-08 13:47:38 -0700

    wv

--- 2018-11-06-ri.py
+++ 2018-11-06-ri.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['New England Man Preparing For Fourth Nor', '3.2 million to the PTP', 'rhode island bill', 'RI State Senator Nicholas Kettle', 'attended the Providence Women', 'Rhode Island, land of more liberals']
+            terms = ['36 million trees a year', 'New England Man Preparing For Fourth Nor', '3.2 million to the PTP', 'rhode island bill', 'RI State Senator Nicholas Kettle', 'attended the Providence Women', 'Rhode Island, land of more liberals']
             for term in terms:
                  search(term, submission);
 
