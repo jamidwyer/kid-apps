6b193a01e182aad436284921a9f354fe8aa744ec
jami <jamidwyer@yahoo.com>
2018-05-12 23:48:11 +0000

    posts

---
+++ ra.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cd /home/ubuntu/running-against-bot
+/usr/bin/python strange.py
+python /home/ubuntu/running-against-bot/2018-02-06-mo.py
+python /home/ubuntu/running-against-bot/2018-02-13-ok.py
+python /home/ubuntu/running-against-bot/2018-03-13-pa.py
+python 2018-11-06-tx.py
+python 2018-11-06-il.py
+python 2018-11-06-oh.py
+python 2018-11-06-nc.py
+python 2018-11-06-pa.py
+python 2018-11-06-sc.py
+python 2018-11-06-nj.py
+python 2018-11-06-nv.py
+python 2018-11-06-ca.py
+python virginia.py
+python 2018-11-06-ok.py
+python larryhogan.py
+python lepage.py
+python 2018-11-06-ut.py
+python 2018-11-06-mi.py
+python 2018-11-06-mo.py
+python 2018-11-06-ks.py
+python 2018-11-06-wa.py
+python 2018-11-06-az.py
+python 2018-11-06-wi.py
