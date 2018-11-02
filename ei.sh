9d2a3ae7bfaefb8febac7849b5c5a8635a360cd7
Jami Dwyer <jamidwyer@yahoo.com>
2018-11-02 13:30:32 -0700

    ga

--- ei.sh
+++ ei.sh
@@ -4,6 +4,7 @@ python 2018-11-06-ga.py #10-15to11-2
 python 2018-11-06-nv.py #10-20to11-2
 python 2018-11-06-tx.py #10-22to11-2
 python 2018-11-06-wv.py #10-24to11-3 senate
+python 2018-11-06-ok.py #11-1to11-3 governor
 python 2018-11-06-hi.py #10-23to11-3
 python 2018-11-06-fl.py #10-22to11-4 senate, governor
 python 2018-11-06-ca.py #11-3to11-4 house
@@ -17,7 +18,6 @@ python 2018-11-06-ak.py #10-22to11-5
 python 2018-11-06-or.py #10-19to11-6
 python 2018-11-06-wa.py #10-29
 python 2018-11-06-ut.py #10-30
-python 2018-11-06-ok.py #11-1to11-3
 python 2018-11-06-az.py #11-2
 python 2018-11-06-mo.py #11-6 senate
 python 2018-11-06-tn.py #11-6 senate
