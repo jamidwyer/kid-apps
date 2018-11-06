d9c18fe904465117a14c6dd39f470c0aa4e6d28e
Jami Dwyer <jamidwyer@yahoo.com>
2018-11-06 05:46:37 -0800

    fl

--- 2018-11-06-fl.py
+++ 2018-11-06-fl.py
@@ -47,7 +47,7 @@ def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
         text = ("Florida 2018 Election \n\n"
-            "[General Election](http://dos.myflorida.com/elections/for-voters/voting/absentee-voting/): November 6, 2018 \n\n")
+            "[General Election](https://registration.elections.myflorida.com/CheckVoterStatus): November 6, 2018 \n\n")
         print("Bot replying to : ", submission.title)
         try:
             submission.reply(text)
