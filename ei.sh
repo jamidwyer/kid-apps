1a97630bd5b44f9065d0590993d1d5ba6dc24a63
Jami Dwyer <jamidwyer@yahoo.com>
2019-03-29 12:57:25 -0700

    sub updates

--- ei.sh
+++ ei.sh
@@ -4,4 +4,4 @@ python 2020-11-03-ny.py # 2019-10-11
 python 2020-11-03-ia.py # 2020-01-24 
 python 2020-11-03-ca.py # 2020-02-17
 python 2020-11-03-ky.py # 2020-04-20
-python 2020-11-03-tx.py # senate
+python 2020-11-03-tx.py # 2020-10-06
