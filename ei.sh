45a24317c96e333ff0a64734ad6b83019c954e50
Jami Dwyer <jamidwyer@yahoo.com>
2019-05-13 11:40:52 -0700

    process checker

--- ei.sh
+++ ei.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd /home/ubuntu/running-against-bot
-if pgrep "python 2019-09-10-nc.py" >/dev/null 2>&1 ; then
+if pgrep -f "python 2019-09-10-nc.py" &>/dev/null; then
     echo "Process already running"
 else
 	python 2019-09-10-nc.py # 2019-08-15
 fi
-if pgrep "python 2020-11-03-ny.py" >/dev/null 2>&1 ; then
+if pgrep -f "python 2020-11-03-ny.py" &>/dev/null; then
     echo "Process already running"
 else
 	python 2020-11-03-ny.py # 2019-10-11
