79ba816ed9397f2680f9275f2aac2db1e553cab3
Jami Dwyer <jamidwyer@yahoo.com>
2019-05-14 13:39:09 -0700

    process check fix maybe

--- ei.sh
+++ ei.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /home/ubuntu/running-against-bot
-if pgrep -fl "^(/.*)?${2019-09-10-nc.py}\s" &>/dev/null; then
-    echo "Process already running"
-else
+if [pgrep -fc "python 2019-09-10-nc.py" &>/dev/null == 0]; then
 	python 2019-09-10-nc.py # 2019-08-15
-fi
-if pgrep -fl "^(/.*)?${2019-09-10-ny.py}\s" &>/dev/null; then
-    echo "Process already running"
 else
+    echo "Process already running"
+fi
+if [pgrep -fc "python 2019-09-10-ny.py" &>/dev/null == 0]; then
 	python 2020-11-03-ny.py # 2019-10-11
+else
+    echo "Process already running"
 fi
 if pgrep -f "python 2020-11-03-ia.py" &>/dev/null; then
     echo "Process already running"
