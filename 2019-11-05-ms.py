fdddf3663e52f3e87100335b8ccd2f1a625d2e8d
Jami Dwyer <jamidwyer@yahoo.com>
2019-07-02 20:28:33 -0700

    ky

--- 2019-11-05-ms.py
+++ 2019-11-05-ms.py
@@ -39,7 +39,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['mississippi goddam', 'dolphins along the gulf coast', 'equal rights amendment', 'mississippi gov', 'ms-gov', 'infant mortality rate', 'ms christian lawmaker', 'doug mcleod', 'mississippi lawmaker punched wife', 'the mississippi house', 'poorest state']
+            terms = ['cut off black farmers', 'mississippi goddam', 'dolphins along the gulf coast', 'equal rights amendment', 'mississippi gov', 'ms-gov', 'infant mortality rate', 'ms christian lawmaker', 'doug mcleod', 'mississippi lawmaker punched wife', 'the mississippi house', 'poorest state']
             for term in terms:
                  search(term, submission);
 
