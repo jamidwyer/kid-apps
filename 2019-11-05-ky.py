5a283f2f2ca879541c04e94664ef562b5e39cdf6
Jami Dwyer <jamidwyer@yahoo.com>
2019-07-31 14:25:55 -0700

    tx

--- 2019-11-05-ky.py
+++ 2019-11-05-ky.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['gain of democratic governors', 'voted in the kentucky primaries', 's most unpopular governor', 'bevin']
+            terms = ['kentucky miners block train', 'gain of democratic governors', 'voted in the kentucky primaries', 's most unpopular governor', 'bevin']
             for term in terms:
                  search(term, submission);
 
