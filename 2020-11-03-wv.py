7a0982e57b9686adbd8e387d5be2755657154154
Jami Dwyer <jamidwyer@yahoo.com>
2019-08-21 19:38:17 -0700

    epstein

--- 2020-11-03-wv.py
+++ 2020-11-03-wv.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['evangelicals chafe at trump', 'racist rally, some christians', 'dea tracked every opioid pill', 'swearengin', 'west virginia expands medical marijuana', 'warren in heart of maga country', 'knock down the house', 'knockdownthehouse', 'basic income mean for appalachia', 'a democrat thing\"', 'rally in west virginia', 'West Virginia District', 'W.Va. Supreme Court', 'West Virginia Supreme Court', 'WV Supreme Court', 'west virginia strike', 'west virginia republican', 'west virginia primary', 'ex-con coal baron', 'wv attorney General', 'senate bid in west virginia', 'Revolt in West Virginia', 'kendra fershee', 'worst state economies in the US', 'democrats in coal country', 'first deregulated bridge', 'wv teachers walkout', 'west virginia teacher strike', 'w.va lawmaker', 'west virginia teachers\'', 'West Virginia politic', 'Trump Can\'t Save Coal', 'lissa lucas', 'west virginia legislat', 'WV House Chambers', 'West Virginia Democratic Candidate', 'West Virginia house hearing', 'town hall in west Virginia', 'wv-sen', 'West Virginia Senate GOP Primary', 'Trump is on his way to West Virginia', 'manchin', 'Richard Ojeda', 'Appalachia poor']
+            terms = ['evangelicals chafe at trump', 'racist rally, some christians', 'dea tracked every opioid pill', 'swearengin', 'west virginia expands medical marijuana', 'west virginia governor', 'warren in heart of maga country', 'knock down the house', 'knockdownthehouse', 'basic income mean for appalachia', 'a democrat thing\"', 'rally in west virginia', 'West Virginia District', 'W.Va. Supreme Court', 'West Virginia Supreme Court', 'WV Supreme Court', 'west virginia strike', 'west virginia republican', 'west virginia primary', 'ex-con coal baron', 'wv attorney General', 'senate bid in west virginia', 'Revolt in West Virginia', 'kendra fershee', 'worst state economies in the US', 'democrats in coal country', 'first deregulated bridge', 'wv teachers walkout', 'west virginia teacher strike', 'w.va lawmaker', 'west virginia teachers\'', 'West Virginia politic', 'Trump Can\'t Save Coal', 'lissa lucas', 'west virginia legislat', 'WV House Chambers', 'West Virginia Democratic Candidate', 'West Virginia house hearing', 'town hall in west Virginia', 'wv-sen', 'West Virginia Senate GOP Primary', 'Trump is on his way to West Virginia', 'manchin', 'Richard Ojeda', 'Appalachia poor']
             for term in terms:
                  search(term, submission);
 
@@ -48,7 +48,9 @@ def search(term, submission):
     if re.search(term, submission.title, re.IGNORECASE):
         # Reply to the post
         text = ("West Virginia 2020 Election \n\n"
-            "[Register to Vote](https://ovr.sos.wv.gov/Register/Landing#Qualifications) \n\n"
+            "[Primary Election Voter Registration](https://ovr.sos.wv.gov/Register/Landing#Qualifications): April 21, 2020 \n\n"
+            "[Primary Election](https://services.sos.wv.gov/Elections/Voter/FindMyPollingPlace): May 12, 2020 \n\n"
+            "[General Election Voter Registration](https://ovr.sos.wv.gov/Register/Landing#Qualifications): October 13, 2020 \n\n"
             "[General Election](https://services.sos.wv.gov/Elections/Voter/FindMyPollingPlace): November 3, 2020 \n\n")
         print("Bot replying to : ", submission.title)
         try:
