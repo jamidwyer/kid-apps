b999abf8ae436f07d0f007636039387623e3814a
Jami Dwyer <jamidwyer@yahoo.com>
2019-08-28 02:16:56 -0700

    var

--- 2020-11-03-va.py
+++ 2020-11-03-va.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['election will hinge on what they do about an impeachment inquiry', 'rep. wexton', 'fake presidential seal']
+            terms = ['Apparently President Obama is still in this guy', 'election will hinge on what they do about an impeachment inquiry', 'rep. wexton', 'fake presidential seal']
             for term in terms:
                  search(term, submission);
 
