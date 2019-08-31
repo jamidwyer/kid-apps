668b5428c7708cd31bf3e1c7c74eb3aebf98022d
Jami Dwyer <jamidwyer@yahoo.com>
2019-08-31 13:41:08 -0700

    or

--- 2020-11-03-hi.py
+++ 2020-11-03-hi.py
@@ -40,7 +40,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['lawsuits in hawaii', 'Ben Shapiro Seriously Claims', 'corals near hawaii', '26th state to decriminalize', 'great pacific garbage patch', 'hawaii has decriminalized', 'legal in Hawaii', 'hawaii legislat', 'Hawaii Ballot', 'kaniela']
+            terms = ['Monsanto used former top DOJ official', 'lawsuits in hawaii', 'Ben Shapiro Seriously Claims', 'corals near hawaii', '26th state to decriminalize', 'great pacific garbage patch', 'hawaii has decriminalized', 'legal in Hawaii', 'hawaii legislat', 'Hawaii Ballot', 'kaniela']
             for term in terms:
                  search(term, submission);
 
