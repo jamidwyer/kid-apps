255ddd9a2dc9ea254c36de1e44623bad2f457414
Jami Dwyer <jamidwyer@yahoo.com>
2019-11-05 19:55:21 -0800

    vky

--- 2020-11-03-va.py
+++ 2020-11-03-va.py
@@ -38,7 +38,7 @@ def searchAndPost(sub):
         if submission.id not in posts_replied_to:
 
             # Do a case insensitive search
-            terms = ['danica roem', 'house of delegates flipped', 'virginia state senate', 'Russia specifically targeted Virginia', '7 national security democrats', 'Apparently President Obama is still in this guy', 'election will hinge on what they do about an impeachment inquiry', 'rep. wexton', 'fake presidential seal']
+            terms = ['red hair boi', 'competitive stafford county', 'lee j. carter', 'danica roem', 'house of delegates flipped', 'virginia state senate', 'Russia specifically targeted Virginia', '7 national security democrats', 'Apparently President Obama is still in this guy', 'election will hinge on what they do about an impeachment inquiry', 'rep. wexton', 'fake presidential seal']
             for term in terms:
                  search(term, submission)
 
