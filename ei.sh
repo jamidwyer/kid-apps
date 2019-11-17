a7bf919282c012a962db190ae2f382055a984ff3
Jami Dwyer <jamidwyer@yahoo.com>
2019-11-16 18:09:54 -0800

    la

--- ei.sh
+++ ei.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd /home/ubuntu/running-against-bot
-	python 2019-11-16-la.py # 2019-11-16
 	python 2020-11-03-ia.py # 2020-01-24
 	python 2020-11-03-sc.py # 2020-01-30
 	python 2020-11-03-ar.py # 2020-02-02
